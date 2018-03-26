459b98f1ad142d3b0168ec5e55e50e1ee948e290
恸暧彝眚 <326416055@qq.com>
2018-03-26 20:01:23 +0800

    updated

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
 $(function(){
    "use strict";
    var qsf = function(){
-      var timer = null;
-       var i = 0;
-       timer = setInterval(function(){
-           i++;
-           if(i>3){
-               i=0;
-           }
-           $("#carousel li").eq(i).show().siblings("#carousel li").hide();
-           console.log(i)
-       },3000);
-      $("#carousel li").mouseover(function(){
-         clearInterval(timer)
-      }).mouseout(function(){
-          timer = setInterval(function(){
-              i++;
-              if(i>3){
-                  i=0;
-              }
-              $("#carousel li").eq(i).show().siblings("#carousel li").hide();
-              console.log(i)
-          },3000);
-
-      })
+      // var timer = null;
+      //  var i = 0;
+      //  timer = setInterval(function(){
+      //      i++;
+      //      if(i>3){
+      //          i=0;
+      //      }
+      //      $("#carousel li").eq(i).show().siblings("#carousel li").hide();
+      //      console.log(i)
+      //  },3000);
+      // $("#carousel li").mouseover(function(){
+      //    clearInterval(timer)
+      // }).mouseout(function(){
+      //     timer = setInterval(function(){
+      //         i++;
+      //         if(i>3){
+      //             i=0;
+      //         }
+      //         $("#carousel li").eq(i).show().siblings("#carousel li").hide();
+      //     },3000);
+      // })
    };
     qsf();
+
+    $('.qsf-adn').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+    $('.carousel').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
 });
